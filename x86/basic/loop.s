43c806b4fe96699cd18b36fd621b2d02d67be9bb
Destinyside <destinyside@163.com>
2018-04-24 22:30:41 +0800

    new loop

--- x86/basic/loop.s
+++ x86/basic/loop.s
@@ -7,13 +7,21 @@ len=.-msg
 .global _start
 
 _start:
+movl $0, %edi
+jmp _loop
+
+_loop:
 movl $len, %edx
 movl $msg, %ecx
 movl $1, %ebx
 movl $4, %eax
 int $0x80
-jmp _start
+incl %edi
+cmpl %edi, %edx
+je _exit
+jmp _loop
 
+_exit:
 movl $0, %ebx
 movl $1, %eax
 int $0x80
